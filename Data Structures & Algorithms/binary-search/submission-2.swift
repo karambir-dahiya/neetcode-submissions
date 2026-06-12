fe42db0053f5eb83ecfc49f5dde147be5119a51b
karambir-dahiya <karmvirdahiya.com@gmail.com>
2026-06-12 05:42:15 +0530

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        if nums.isEmpty {
+            return -1
+        }
+        var left = 0
+        var right = nums.count - 1
+        while(left <= right) {
+            if(nums[left] == target) {
+                return left
+            } else if(nums[right] == target) {
+                return right
+            } else {
+                left += 1
+                right -= 1
+            }
+        }
+        return -1
+    }
+}
