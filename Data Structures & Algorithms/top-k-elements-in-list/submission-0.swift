0faacc00f160a3f88ec7f4e8a0a4c3aed9899072
karambir-dahiya <karmvirdahiya.com@gmail.com>
2026-06-12 08:51:14 +0530

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+        if nums.isEmpty {
+            return []
+        }
+
+        var dict = [Int:Int]()
+        for num in nums {
+            dict[num,default:0] += 1
+        }
+
+        let sorted = dict.sorted {$0.value > $1.value}
+        return Array(sorted.prefix(k).map { $0.key })
+    }
+}
